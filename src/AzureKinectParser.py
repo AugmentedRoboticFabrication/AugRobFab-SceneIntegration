4f8d4da79678d2c52f1e700de1084c20e9720e58
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2023-03-27 10:58:05 -0400

    Revert "pyk4a based MKV Parser"
    This reverts commit 300681effea767bbdb4a4c27106755df191729f6.

--- src/AzureKinectParser.py
+++ src/AzureKinectParser.py
@@ -1,9 +1,6 @@
 import os
 import open3d as o3d
-import cv2
 import numpy as np
-import pyk4a
-from pyk4a import PyK4APlayback
 
 class AzureKinectMKVParser(object):
 	def __init__(self, fn, root=None):
@@ -14,80 +11,57 @@ class AzureKinectMKVParser(object):
 		else:
 			self.root = root
 		
-		self.mkv = '%s/%s/capture.mkv' %(self.root, self.fn)
-		self.playback = PyK4APlayback(self.mkv)
+		self.mkv = '%s\\%s\\capture.mkv' %(self.root, self.fn)
+		self.playback = None
 		self.reader = o3d.io.AzureKinectMKVReader()
 
-		self.reader.open(self.mkv)
-		metadata = self.reader.get_metadata()
-		o3d.io.write_azure_kinect_mkv_metadata('%s/%s/intrinsic.json' %(self.root, self.fn), metadata)
-		self.reader.close()
+	def rgbd(self, color=False, depth=False):
+		# frames = []
 
-	def rgbd(self, depth=True, color=False, ir=False):
-		self.playback.open()
-		print(os.path.exists(self.fn))
-		if color and not os.path.exists('%s/color' % self.fn):
-			os.mkdir('%s/color' % self.fn)
+		if color and not os.path.exists('%s\\%s\\color' %(self.root, self.fn)):
+			os.mkdir('%s\\%s\\color' %(self.root, self.fn))
 		
-		if depth and not os.path.exists('%s/depth' % self.fn):
-			os.mkdir('%s/depth' % self.fn)
+		if depth and not os.path.exists('%s\\%s\\depth' %(self.root, self.fn)):
+			os.mkdir('%s\\%s\\depth' %(self.root, self.fn))
 
-		if ir and not os.path.exists('%s/ir' % self.fn):
-			os.mkdir('%s/ir' % self.fn)
+		self.reader.open(self.mkv)
+
+		metadata = self.reader.get_metadata()
+		o3d.io.write_azure_kinect_mkv_metadata('%s\\%s\\intrinsic.json' %(self.root, self.fn), metadata)
 
 		index = 0
-		while True:
-			try:
-				capture = self.playback.get_next_capture()
-				if color and capture.color is not None:
-					im_color = cv2.imdecode(capture.color, cv2.IMREAD_UNCHANGED)
-					im_color = cv2.cvtColor(im_color, cv2.COLOR_RGB2RGBA)
+		while not self.reader.is_eof():
+			rgbd = self.reader.next_frame()
+			if rgbd is None:
+				continue
+			# frames.append(rgbd)
 
-					color_fn = '%s/color/%03d.png' % ( self.fn, index)
-					cv2.imwrite(color_fn, im_color)
+			if color:
+				color_fn = '%s\\%s\\color\\%03d.jpg' % (self.root, self.fn, index)
+				o3d.io.write_image(color_fn, rgbd.color)
 
-				if depth and capture.depth is not None:
-					depth_fn = '%s/depth/%03d.png' % ( self.fn, index)
-					cv2.imwrite(depth_fn, capture.depth)
-
-				if ir and capture.ir is not None:
-					ir_fn = '%s/ir/%03d.png' % (self.fn, index)
-					cv2.imwrite(ir_fn, capture.ir)
-				index += 1
-			except EOFError:
-				break
-		self.playback.close()
+			if depth:
+				depth_fn = '%s\\%s\\depth\\%03d.png' % (self.root, self.fn, index)
+				o3d.io.write_image(depth_fn, rgbd.depth)
+			index += 1
 		
+		self.reader.close()
 
 	def calibration(self):
+		os.add_dll_directory("C:\\Program Files\\Azure Kinect SDK v1.4.1\\sdk\\windows-desktop\\amd64\\release\\bin")
+		import pyk4a
+		from pyk4a import PyK4APlayback
+
+		self.playback = PyK4APlayback(self.mkv)
+		print('Exporting factory calibration:')
 		self.playback.open()
 
-		# mtx = self.playback.calibration.get_camera_matrix(pyk4a.calibration.CalibrationType.COLOR).tolist()
-		# dist = self.playback.calibration.get_distortion_coefficients(pyk4a.calibration.CalibrationType.COLOR).tolist()
-		# print(np.asarray(self.playback.calibration.get_camera_matrix(pyk4a.calibration.CalibrationType.DEPTH).tolist()).T.flatten())
-		# np.savez('%s/%s/calibration' %(self.root, self.fn), mtx=mtx, dist=dist)
+		mtx = self.playback.calibration.get_camera_matrix(pyk4a.calibration.CalibrationType.COLOR).tolist()
+		dist = self.playback.calibration.get_distortion_coefficients(pyk4a.calibration.CalibrationType.COLOR).tolist()
 
-		# print('Camera Matrix:\n', mtx)
-		# print('Distortion Coefficients:\n',dist)
+		np.savez('%s\\%s\\calibration' %(self.root, self.fn), mtx=mtx, dist=dist)
 
-		self.playback.close()
-
-	def overwriteIntrinsic(self):
-		import json
-
-		self.playback.open()
-
-		intrinsic = np.asarray(self.playback.calibration.get_camera_matrix(pyk4a.calibration.CalibrationType.DEPTH).tolist()).T.flatten()
-		with open('%s/%s/intrinsic.json' % (self.root, self.fn), "r+") as f:
-			data = json.load(f)
-			data["intrinsic_matrix"] = intrinsic.tolist()
-			capture = self.playback.get_next_capture()
-			data["width"] = capture.depth.shape[0]
-			data["height"] = capture.depth.shape[1]
-			f.seek(0)
-			json.dump(data, f)
-			f.truncate()
-		# print('Camera Matrix:\n', mtx)
-		# print('Distortion Coefficients:\n',dist)
+		print('Camera Matrix:\n', mtx)
+		print('Distortion Coefficients:\n',dist)
 
 		self.playback.close()
